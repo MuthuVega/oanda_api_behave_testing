30d6ae200a089492783f809df46b33289271cd94
MuthuVega <muthu_hcl@yahoo.com>
2023-12-20 15:01:46 +0000

    feat: Add code linting using black

--- features/environment.py
+++ features/environment.py
@@ -7,4 +7,3 @@ def before_all(context):
     utils.set_base_url(context)
     utils.set_headers(context)
     logging.info(f"Vale loaded into context is {context}")
-
--- features/steps/modules/utils/api_endpoints.py
+++ features/steps/modules/utils/api_endpoints.py
@@ -4,11 +4,26 @@ Module to store API endpoint details
 
 OANDA_API_ENDPOINTS = {
     "GET_ACCOUNTS": {"endpoint": "/v3/accounts", "type": "GET", "params": None},
-    "GET_ACCOUNT_DETAILS": {"endpoint": "/v3/accounts/{accountID}", "type": "GET", "params": ["accountID"]},
-    "GET_ACCOUNT_SUMMARY": {"endpoint": "/v3/accounts/{accountID}/summary", "type": "GET", "params": ["accountID"]},
-    "GET_ACCOUNT_INSTRUMENTS": {"endpoint": "/v3/accounts/{accountID}/instruments", "type": "GET",
-                                "params": ["accountID"]},
-    "GET_ACCOUNT_CHANGES": {"endpoint": "/v3/accounts/{accountID}/changes?sinceTransactionID={{transactionID}}", "type": "GET", "params": ["accountID", "transactionID"]},
+    "GET_ACCOUNT_DETAILS": {
+        "endpoint": "/v3/accounts/{accountID}",
+        "type": "GET",
+        "params": ["accountID"],
+    },
+    "GET_ACCOUNT_SUMMARY": {
+        "endpoint": "/v3/accounts/{accountID}/summary",
+        "type": "GET",
+        "params": ["accountID"],
+    },
+    "GET_ACCOUNT_INSTRUMENTS": {
+        "endpoint": "/v3/accounts/{accountID}/instruments",
+        "type": "GET",
+        "params": ["accountID"],
+    },
+    "GET_ACCOUNT_CHANGES": {
+        "endpoint": "/v3/accounts/{accountID}/changes?sinceTransactionID={{transactionID}}",
+        "type": "GET",
+        "params": ["accountID", "transactionID"],
+    },
 }
 
 
