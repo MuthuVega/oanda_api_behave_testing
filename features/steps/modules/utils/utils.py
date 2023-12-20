d9541302f1e551e8b287cffa19ddbd3764b07740
MuthuVega <muthu_hcl@yahoo.com>
2023-12-20 17:24:29 +0000

    fix: Fix config path

--- features/steps/modules/utils/utils.py
+++ features/steps/modules/utils/utils.py
@@ -11,7 +11,9 @@ CAT_FACT_API = "https://catfact.ninja/fact"
 CONFIG_FILE = (
     "/Users/muthupandianvadivelu/muthu/learn/oanda_api_behave_testing/config.toml"
 )
-
+CONFIG_FILE = (
+    "./config.toml"
+)
 
 def get_cat_fact():
     return requests.get(CAT_FACT_API).json()
